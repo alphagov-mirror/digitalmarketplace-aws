4e1dddc05eb2cdab4b06bb627e8f10f6d4b37a04
Robert Scott <robert.scott@digital.cabinet-office.gov.uk>
2020-01-29 13:53:15 +0000

    terraform: add paulavalenca user

--- terraform/modules/iam-users/users/main.tf
+++ terraform/modules/iam-users/users/main.tf
@@ -37,3 +37,9 @@ resource "aws_iam_user" "alistair_laing" {
   name          = "alistairlaing"
   force_destroy = true
 }
+
+resource "aws_iam_user" "paula_valenca" {
+  name          = "paulavalenca"
+  force_destroy = true
+}
+
